1288a6a9d3f266f12ef84765f1a303f2aae894aa
dilijance <dilijance11@gmail.com>
2023-09-04 11:48:38 +0900

    123

--- pytouch_main/torch1.py
+++ pytouch_main/torch1.py
@@ -5,16 +5,12 @@ class Torch:
         self.name = name
 
     def print(self):
-        print("this is torch class " + self.name)
+        print("this is torch class\n이름은: " + self.name)
 
 def main():
     print("this is my first python programming")
     torch1 = Torch("Chang Mak Sim")
     torch2 = Torch("Choi Al li Na")
-    #torch3 = Torch()
-    #torch4 = Torch()
-    print(torch1.name)
-    print(torch2.name)
 
 if __name__ == "__main__":
     main()
